62a110a6cecbd87f9d9d33d5d254179ed6b6261f
bcl <barrylui88@googlemail.com>
2023-02-06 00:40:47 +0000

    Refactored Question and Answers into arrays and changed how questions are displayed

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -4,139 +4,87 @@ let currentQuestion = document.querySelector("#question-title");
 let choices = document.querySelector("#choices");
 let startButton = document.querySelector("#start");
 let startScreen = document.querySelector("#start-screen");
+let answerChoice1 = document.querySelector("#choice1");
+let answerChoice2 = document.querySelector("#choice2");
+let answerChoice3 = document.querySelector("#choice3");
+let answerChoice4 = document.querySelector("#choice4");
+let currentQuestionIndex = 0;
+let currentAnswerChoice1 = 0;
+let currentAnswerChoice2 = 1;
+let currentAnswerChoice3 = 2;
+let currentAnswerChoice4 = 3;
+let currentQuestionNumber = 1;
 
 // Questions
-let q1;
-let q2;
-let q3;
-let q4;
-let q5;
-let q6;
-let q7;
-let q8;
-let q9;
-let q10;
-let q11;
-let q12;
-let q13;
-let q14;
-let q15;
-let q16;
-let q17;
-let q18;
-let q19;
-let q20;
+let questionArray = [
+    "1. Inside which HTML element do we put the JavaScript?",
+    "2. Hi",
+    "3. ",
+    "4. ",
+    "5. ",
+    "6. ",
+    "7. ",
+    "8. ",
+    "9. ",
+    "10. ",
+    "11. ",
+    "12. ",
+    "13. ",
+    "14. ",
+    "15. ",
+    "16. ",
+    "17. ",
+    "18. ",
+    "19. ",
+    "20. "
+]
 
 // Answer Choices
-let q1a1;
-let q1a2;
-let q1a3;
-let q1a4;
-let q2a1;
-let q2a2;
-let q2a3;
-let q2a4;
-let q3a1;
-let q3a2;
-let q3a3;
-let q3a4;
-let q4a1;
-let q4a2;
-let q4a3;
-let q4a4;
-let q5a1;
-let q5a2;
-let q5a3;
-let q5a4;
-let q6a1;
-let q6a2;
-let q6a3;
-let q6a4;
-let q7a1;
-let q7a2;
-let q7a3;
-let q7a4;
-let q8a1;
-let q8a2;
-let q8a3;
-let q8a4;
-let q9a1;
-let q9a2;
-let q9a3;
-let q9a4;
-let q10a1;
-let q10a2;
-let q10a3;
-let q10a4;
-let q11a1;
-let q11a2;
-let q11a3;
-let q11a4;
-let q12a1;
-let q12a2;
-let q12a3;
-let q12a4;
-let q13a1;
-let q13a2;
-let q13a3;
-let q13a4;
-let q14a1;
-let q14a2;
-let q14a3;
-let q14a4;
-let q15a1;
-let q15a2;
-let q15a3;
-let q15a4;
-let q16a1;
-let q16a2;
-let q16a3;
-let q16a4;
-let q17a1;
-let q17a2;
-let q17a3;
-let q17a4;
-let q18a1;
-let q18a2;
-let q18a3;
-let q18a4;
-let q19a1;
-let q19a2;
-let q19a3;
-let q19a4;
-let q20a1;
-let q20a2;
-let q20a3;
-let q20a4;
+let answerChoicesArray = [
+    "<javascript>","<script>","<scripting>","<js>",
+    "1","2","3","4"
+]
 
 // Correct Answers
-let q1ca;
-let q2ca;
-let q3ca;
-let q4ca;
-let q5ca;
-let q6ca;
-let q7ca;
-let q8ca;
-let q9ca;
-let q10ca;
-let q11ca;
-let q12ca;
-let q13ca;
-let q14ca;
-let q15ca;
-let q16ca;
-let q17ca;
-let q18ca;
-let q19ca;
-let q20ca;
+let correctAnswersArray = [
+    "<script>",
+    "3"
+]
 
 // Event Listeners
 startButton.addEventListener("click",showQuestionSection)
+answerChoice1.addEventListener("click",evaluateAnswer)
+answerChoice2.addEventListener("click",evaluateAnswer)
+answerChoice3.addEventListener("click",evaluateAnswer)
+answerChoice4.addEventListener("click",evaluateAnswer)
 
 // Functions
 function showQuestionSection () {
     startScreen.classList.add("hide");
     questionSection.classList.remove("hide");
-    currentQuestion.textContent = q1
+    showNextQuestion();
 }
+
+function showNextQuestion () {
+    currentQuestion.textContent = questionArray[currentQuestionIndex];
+    choices.children[0].textContent = "1. " + answerChoicesArray[currentAnswerChoice1];
+    choices.children[1].textContent = "2. " + answerChoicesArray[currentAnswerChoice2];
+    choices.children[2].textContent = "3. " + answerChoicesArray[currentAnswerChoice3];
+    choices.children[3].textContent = "4. " + answerChoicesArray[currentAnswerChoice4];
+    currentQuestionIndex++;
+    currentAnswerChoice1 += 4;
+    currentAnswerChoice2 += 4;
+    currentAnswerChoice3 += 4;
+    currentAnswerChoice4 += 4;
+}
+
+function evaluateAnswer () {
+    console.log(currentQuestionNumber);
+    switch (currentQuestionNumber) {
+        case 1:
+            
+        case 2:
+            
+    }
+}
+
